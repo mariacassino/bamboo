292011a3e13d7093528c49e76d21376ec66e7d27
Maria Cassino <maria.cassino@gmail.com>
2016-07-05 19:21:44 -0400

    edited Items controller

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,8 @@ ActiveRecord::Schema.define(version: 20160705204702) do
     t.text     "description"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.decimal  "price",       precision: 8, scale: 2
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
     t.string   "shop_id"
+    t.decimal  "price",       precision: 8, scale: 2
   end
 
   create_table "sellers", force: :cascade do |t|
