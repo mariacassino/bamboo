feac4c6fe65b0822c7291fabf6416ed3cdf673d4
Maria Cassino <maria.cassino@gmail.com>
2016-07-05 21:33:10 -0400

    added description column to Shop model

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -16,11 +16,48 @@ class ShopsController < ApplicationController
       #Create objects here from each row
       row
     end
-
   end
 
   def test
   end
+
+  def new
+    @shop = Shop.new
+    # @shop.user_id = current_user.id
+  end
+
+  def create
+    @shop = Shop.new
+    @shop.user_id = current_user.id
+    if @shop.save
+      flash[:notice] = "Success!"
+      redirect_to @shop
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @shop = Shop.find(params[:id])
+  end
+
+  def update
+    @shops = Shop.all
+    @shop = Shop.find(params[:id])
+    if @shop.update
+      flash[:notice] = "Success!"
+      redirect_to shops_path
+    else
+      redirect_to shops_path
+    end
+  end
+
+  def destroy
+    @shop = Shop.find(params[:id])
+    @board.destroy
+    redirect_to shops_path
+  end
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705215922) do
+ActiveRecord::Schema.define(version: 20160706013136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20160705215922) do
     t.text     "description"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.decimal  "price",       precision: 8, scale: 2
     t.string   "shop_id"
+    t.decimal  "price",       precision: 8, scale: 2
     t.integer  "user_id"
   end
 
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 20160705215922) do
   create_table "shops", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
