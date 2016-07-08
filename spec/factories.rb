e235c9648e24f20aec05d25f5de5a7c8a0fa8323
vegajdr <jorgevp5@gmail.com>
2016-07-08 09:52:07 -0400

    Added FactoryGirl

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    email "email@example.com"
+    password "hunter2"
+  end
+
+  factory :item do
+
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,4 +58,6 @@ RSpec.configure do |config|
   config.before :each, type: :controller do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
+  config.include FactoryGirl::Syntax::Methods
+
 end
