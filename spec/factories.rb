ed598e15c8287bb2f082b6f9e13f6723676d74d4
vegajdr <jorgevp5@gmail.com>
2016-07-08 11:55:42 -0400

    Factory fix

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,10 +9,29 @@ FactoryGirl.define do
     end
   end
 
+
   factory :item do
     name Faker::Beer.name
     description Faker::Beer.style
     price 5.00
     user
+
   end
+
+
+  factory :admin, class: User do
+    email "admin@example.com"
+    password "password"
+    admin true
+  end
+
+
+  factory :shop do
+    name "User Shop"
+    description "my awesome shop"
+    location "my town"
+    user
+  end
+
+
 end
