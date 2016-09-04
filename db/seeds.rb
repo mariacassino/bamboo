2d02c6bcd06e90a864b1f260c4ffe70e3d8ed343
Maria Cassino <maria.cassino@gmail.com>
2016-09-04 14:37:07 -0400

    reseeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-maria = User.create(email: "maria.cassino@gmail.com", password: "password", admin: true)
-vega = User.create(email: "jorgevp5@gmail.com", password: "password", admin: true)
+User.delete_all
+Shop.delete_all
+Item.delete_all
 
-maria.shops.create!(name: "Maria's Awesome Shop", location: "Raleigh, NC")
-vega.shops.create!(name: "Vega's Box of Wonders", location: "Valhalla")
+maria = User.create!(email: "maria.cassino@gmail.com", password: "myproject", admin: true)
+vega = User.create!(email: "jorgevp5@gmail.com", password: "blahblah", admin: false)
 
+marias_shop = maria.shops.create!(name: "Maria's Scented Candles", description: "Delicious Smells!", location: "Raleigh, NC")
+vegas_shop = vega.shops.create!(name: "Vega's Box of Wonders", description: "Mysterious Things!", location: "Valhalla")
 
-maria.items.create!(name: "Solid Gold Aardwolf Statue", description: "magickal talisman", shop_id: 1, price: 159.99)
-vega.items.create!(name: "Ocarina", description: "Let's you travel through time", shop_id: 2, price: 99.99)
+maria.items.create!(name: "Blueberry Candle", description: "Full of antioxidants for your nose.", shop_id: marias_shop.id, price: 3.99)
+maria.items.create!(name: "Cocoa Candle", description: "Dark chocolate scented.", shop_id: marias_shop.id, price: 4.99)
+
+vega.items.create!(name: "Solid Gold Aardwolf Statue", description: "Magickal talisman.", shop_id: vegas_shop.id, price: 159.99)
+vega.items.create!(name: "Ocarina", description: "Lets you travel through time", shop_id: vegas_shop.id, price: 99.99)
