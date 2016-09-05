282fa3c061b1bd4db64fe20d89805575319f8831
Maria Cassino <maria.cassino@gmail.com>
2016-09-04 20:44:44 -0400

    compiled public domain photos for items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706222852) do
+ActiveRecord::Schema.define(version: 20160905002859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160706222852) do
     t.integer  "shop_id"
     t.decimal  "price",       precision: 8, scale: 2
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "sellers", force: :cascade do |t|
