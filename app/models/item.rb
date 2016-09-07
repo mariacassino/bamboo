8fd867603235f98fceb6cc1f619cdf0bde49eba3
Maria Cassino <maria.cassino@gmail.com>
2016-09-07 11:49:05 -0400

    created new Proc in Item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,16 @@ class Item < ActiveRecord::Base
   belongs_to :shop
   belongs_to :seller
   belongs_to :user
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/:style/missing.png",
+    storage: :s3,
+    s3_credentials: Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def s3_credentials
+  {:bucket => ENV["S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+  :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+end
 
 
   def stripe_amount
