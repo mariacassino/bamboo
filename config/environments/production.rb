8113f3c4eafe1bfd65dadee522e18c1d01a93305
vegajdr <jorgevp5@gmail.com>
2016-09-07 16:45:15 -0400

    Changed aws region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :s3_region => 'us-east-1',
+      :s3_region => 'us-west-2',
       :bucket => 'bambooapp',
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
