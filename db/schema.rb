59716ee5fc9933310babb61c246da074a0c6e211
Maria Cassino <maria.cassino@gmail.com>
2016-09-06 20:07:39 -0400

    added 'Image' column to Items table with Paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905005730) do
+ActiveRecord::Schema.define(version: 20160907000426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,15 @@ ActiveRecord::Schema.define(version: 20160905005730) do
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.integer  "shop_id"
-    t.decimal  "price",       precision: 8, scale: 2
+    t.decimal  "price",              precision: 8, scale: 2
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "sellers", force: :cascade do |t|
